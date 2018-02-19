c8ab06a92b934f87b165f298f1e5eafeb5f8a6c2
Jesse Newland <jesse@jnewland.com>
2018-02-19 12:45:47 -0600

    Allow custom dimming amounts

--- python_scripts/relative_dimmer.py
+++ python_scripts/relative_dimmer.py
@@ -1,22 +1,28 @@
-""" Change a light's brightness """
+""" Dim or restore a light by a customizable amount """
 
-entity_ids = data.get('entity_id')
-change     = int(data.get('change'))
+entity_ids = data.get("entity_id")
+action     = data.get("action", "dim")
 
-for entity_id in entity_ids.split(','):
+if action == "dim":
+    multiplier = -1
+elif action == "restore":
+    multipier = 1
+
+for entity_id in entity_ids.split(","):
     state      = hass.states.get(entity_id)
-    brightness = state.attributes.get('brightness') or 0
+    change     = state.attributes.get("dim_amount", 127)
+    brightness = state.attributes.get("brightness", 0)
 
-    dim = (brightness + change)
+    dim = (brightness + (int(change) * int(multiplier)))
 
     if dim >= 254:
         dim = 254
 
     if dim <= 0:
-        logger.info('Tuning off ' + str(entity_id))
+        logger.info("Tuning off " + str(entity_id))
         data = { "entity_id" : entity_id }
-        hass.services.call('light', 'turn_off', data)
+        hass.services.call("light", "turn_off", data)
     else:
-        logger.info('Dimming ' + str(entity_id) + ' from ' + str(brightness) + ' to ' + str(dim))
+        logger.info("Dimming " + str(entity_id) + " from " + str(brightness) + " to " + str(dim))
         data = { "entity_id" : entity_id, "brightness" : dim }
-        hass.services.call('light', 'turn_on', data)
+        hass.services.call("light", "turn_on", data)
