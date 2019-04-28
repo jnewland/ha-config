957ac3a7093f8aabcfce5c4079d5035b63031821
Jesse Newland <jesse@jnewland.com>
2019-04-28 10:15:31 -0500

    everything is a list

--- python_scripts/close_if_open.py
+++ python_scripts/close_if_open.py
@@ -1,10 +1,11 @@
 """ Close a cover only if it is open """
 
-entity_id = data.get("entity_id")
-state = hass.states.get(entity_id)
+entities = data.get("entity_id")
+for entity_id in entities:
+    state = hass.states.get(entity_id)
 
-if state == "open":
-    logger.info("Closing " + str(entity_id))
-    hass.services.call("cover", "close", { "entity_id": entity_id })
-else:
-    logger.info("Not closing " + str(entity_id) + " since it was already closed")
+    if state == "open":
+        logger.info("Closing " + str(entity_id))
+        hass.services.call("cover", "close", { "entity_id": entity_id })
+    else:
+        logger.info("Not closing " + str(entity_id) + " since it was already closed")
