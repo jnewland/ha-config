4e26b8c1d7d931bd0d3a3d689a98c2c458b8f8e3
Jesse Newland <jesse@jnewland.com>
2019-04-28 10:08:22 -0500

    simplify

--- python_scripts/close_if_open.py
+++ python_scripts/close_if_open.py
@@ -1,13 +1,10 @@
 """ Close a cover only if it is open """
 
-entity_ids = data.get("entity_id")
+entity_id = data.get("entity_id")
+state = hass.states.get(entity_id)
 
-for entity_id in entity_ids.split(","):
-    entity_id = entity_id.replace(" ", "")
-    state = hass.states.get(entity_id)
-
-    if state == "open":
-        logger.info("Closing " + str(entity_id))
-        hass.services.call("cover", "close", { "entity_id": entity_id })
-    else:
-        logger.info("Not closing " + str(entity_id) + " since it was already closed")
+if state == "open":
+    logger.info("Closing " + str(entity_id))
+    hass.services.call("cover", "close", { "entity_id": entity_id })
+else:
+    logger.info("Not closing " + str(entity_id) + " since it was already closed")
