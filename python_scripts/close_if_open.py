fbc8d663a067055042aca8af85117f79ce462fa0
Jesse Newland <jesse@jnewland.com>
2019-04-28 11:08:35 -0500

    Update close_if_open.py

--- python_scripts/close_if_open.py
+++ python_scripts/close_if_open.py
@@ -6,6 +6,6 @@ for entity_id in entities:
 
     if state.state == "open":
         logger.info("Closing " + str(entity_id))
-        hass.services.call("cover", "close", { "entity_id": entity_id })
+        hass.services.call("cover", "close_cover", { "entity_id": entity_id })
     else:
         logger.info("Not closing " + str(entity_id) + " since it was " + state.state)
