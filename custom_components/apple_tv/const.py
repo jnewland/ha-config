4bb72912dddf20b37ca1e4f3553dfca7778a3cc6
Jesse Newland <jesse@jnewland.com>
2020-01-03 20:46:24 -0600

    postlund/hass-atv-beta@4a627fe8cc30d8506e1cdc92f67b9bb80ff2fc37

---
+++ custom_components/apple_tv/const.py
@@ -0,0 +1,11 @@
+"""Constants for the Apple TV integration."""
+
+DOMAIN = "apple_tv"
+
+CONF_IDENTIFIER = "identifier"
+CONF_PROTOCOL = "protocol"
+CONF_CREDENTIALS = "credentials"
+
+CONF_START_OFF = "start_off"
+
+KEY_MANAGER = "appletv_manager"
