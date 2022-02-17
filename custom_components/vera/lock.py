fb7e4c983d806d6a934dd24c95f5846410ef7ec4
Jesse Newland <jesse@jnewland.com>
2022-02-17 17:03:50 -0600

    Sketch visitor mode (#416)
    * Test updated vera integration
    
    * required
    
    * pin
    
    * service targets
    
    * cleanup
    
    * len
    
    * update
    
    * debug
    
    * quiet
    
    * fix api, use fork maybe
    
    * hmm
    
    * okay
    
    * quiet for now
    
    * status
    
    * status
    
    * sketch out visitor mode
    
    * polish
    
    * int
    
    * more loops
    
    * switch to input_text
    
    * no slots
    
    * input_text
    
    * better camera
    
    * coerce
    
    * adjust notification timing
    
    * text only, rename
    
    * notify

---
+++ custom_components/vera/lock.py
@@ -0,0 +1,193 @@
+"""Support for Vera locks."""
+from __future__ import annotations
+
+from http import HTTPStatus
+import logging
+from typing import Any
+
+import pyvera as veraApi
+import voluptuous as vol
+
+from homeassistant.components.lock import ENTITY_ID_FORMAT, LockEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import (
+    ATTR_CREDENTIALS,
+    CONF_COMMAND_STATE,
+    CONF_NAME,
+    CONF_PIN,
+    STATE_LOCKED,
+    STATE_OK,
+    STATE_UNLOCKED,
+    Platform,
+)
+from homeassistant.core import HomeAssistant
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.entity_platform import (
+    AddEntitiesCallback,
+    async_get_current_platform,
+)
+
+from . import VeraDevice
+from .common import ControllerData, get_controller_data
+
+_LOGGER = logging.getLogger(__name__)
+
+ATTR_LAST_USER_NAME = "changed_by_name"
+ATTR_LOW_BATTERY = "low_battery"
+SET_PIN_SCHEMA = cv.make_entity_service_schema(
+    {
+        vol.Required(CONF_NAME): cv.string,
+        vol.Required(CONF_PIN): cv.string,
+    }
+)
+CLEAR_PIN_SCHEMA = cv.make_entity_service_schema(
+    {
+        vol.Optional(CONF_NAME): cv.string,
+    }
+)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+) -> None:
+    """Set up the sensor config entry."""
+    controller_data = get_controller_data(hass, entry)
+    platform = async_get_current_platform()
+    platform.async_register_entity_service(
+        name="set_lock_pin", schema=SET_PIN_SCHEMA, func=VeraLock.set_lock_pin.__name__
+    )
+    platform.async_register_entity_service(
+        name="clear_lock_pin",
+        schema=CLEAR_PIN_SCHEMA,
+        func=VeraLock.clear_lock_pin.__name__,
+    )
+
+    async_add_entities(
+        [
+            VeraLock(device, controller_data)
+            for device in controller_data.devices[Platform.LOCK]
+        ],
+        True,
+    )
+
+
+class VeraLock(VeraDevice[veraApi.VeraLock], LockEntity):
+    """Representation of a Vera lock."""
+
+    def __init__(
+        self, vera_device: veraApi.VeraLock, controller_data: ControllerData
+    ) -> None:
+        """Initialize the Vera device."""
+        self._state: str | None = None
+        self._cmd_status: str | None = None
+        VeraDevice.__init__(self, vera_device, controller_data)
+        self.entity_id = ENTITY_ID_FORMAT.format(self.vera_id)
+
+    def lock(self, **kwargs: Any) -> None:
+        """Lock the device."""
+        self.vera_device.lock()
+        self._state = STATE_LOCKED
+
+    def unlock(self, **kwargs: Any) -> None:
+        """Unlock the device."""
+        self.vera_device.unlock()
+        self._state = STATE_UNLOCKED
+
+    def set_lock_pin(self, **kwargs: Any) -> None:
+        """Set pin on the device."""
+        _LOGGER.debug("calling veralock.set_lock_pin")
+        result = self.vera_device.set_new_pin(
+            name=kwargs[CONF_NAME],
+            # fix upstream to accept strings
+            pin=int(kwargs[CONF_PIN]),
+        )
+        if result.status_code == HTTPStatus.OK:
+            self._cmd_status = STATE_OK
+        else:
+            self._cmd_status = result.text
+            _LOGGER.error(
+                "Failed to call %s: (%d) %s",
+                "veralock.set_lock_pin",
+                result.status_code,
+                result.text,
+            )
+            raise ValueError(result.text)
+
+    def clear_lock_pin(self, **kwargs: Any) -> None:
+        """Clear pin on the device."""
+        _LOGGER.debug("calling veralock.clear_lock_pin")
+        credentials = self.vera_device.get_pin_codes()
+        if credentials is None:
+            self._cmd_status = STATE_OK
+            return
+
+        found_slot = None
+        for slot, name, _ in credentials:
+            if name == kwargs["name"]:
+                found_slot = slot
+        if found_slot is None:
+            self._cmd_status = STATE_OK
+            return
+
+        result = self.vera_device.clear_slot_pin(slot=found_slot)
+        if result.status_code == HTTPStatus.OK:
+            self._cmd_status = STATE_OK
+        else:
+            self._cmd_status = result.text
+            _LOGGER.error(
+                "Failed to call %s: (%d) %s",
+                "veralock.clear_lock_pin",
+                result.status_code,
+                result.text,
+            )
+            raise ValueError(result.text)
+
+    @property
+    def is_locked(self) -> bool | None:
+        """Return true if device is on."""
+        return self._state == STATE_LOCKED
+
+    @property
+    def extra_state_attributes(self) -> dict[str, Any] | None:
+        """Who unlocked the lock and did a low battery alert fire.
+
+        Reports on the previous poll cycle.
+        changed_by_name is a string like 'Bob'.
+        low_battery is 1 if an alert fired, 0 otherwise.
+        """
+        data = super().extra_state_attributes or {}
+
+        last_user = self.vera_device.get_last_user_alert()
+        if last_user is not None:
+            data[ATTR_LAST_USER_NAME] = last_user[1]
+
+        data[ATTR_LOW_BATTERY] = self.vera_device.get_low_battery_alert()
+
+        credentials = self.vera_device.get_pin_codes()
+        if credentials is not None:
+            data[
+                ATTR_CREDENTIALS
+            ] = f"{','.join(str(name) for _slot, name, _pin in credentials)}"
+
+        status = self._cmd_status
+        if status is not None:
+            data[CONF_COMMAND_STATE] = self._cmd_status
+        return data
+
+    @property
+    def changed_by(self) -> str | None:
+        """Who unlocked the lock.
+
+        Reports on the previous poll cycle.
+        changed_by is an integer user ID.
+        """
+        last_user = self.vera_device.get_last_user_alert()
+        if last_user is not None:
+            return last_user[0]
+        return None
+
+    def update(self) -> None:
+        """Update state by the Vera device callback."""
+        self._state = (
+            STATE_LOCKED if self.vera_device.is_locked(True) else STATE_UNLOCKED
+        )
