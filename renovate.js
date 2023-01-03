87158e9ba47727ec9cecda9bbdc499509a99be81
Jesse Newland <jesse@jnewland.com>
2023-01-03 11:19:44 -0500

    use app token to workaround rate limit

--- renovate.js
+++ renovate.js
@@ -1,20 +1,24 @@
 module.exports = {
-  "$schema": "https://docs.renovatebot.com/renovate-schema.json",
-  "username": "jnewland-renovate[bot]",
-  "globalExtends": ["github>jnewland/.github"],
-  "postUpgradeTasks": {
-    "commands": ["./script/sync-components"],
-    "fileFilters": ["custom_components/**/*"],
-    "executionMode": "branch"
-  },
-  "packageRules": [
+  globalExtends: ['github>jnewland/.github'],
+  hostRules: [
     {
-      "matchPackageNames": [
-        "homeassistant/home-assistant",
-        "homeassistant/core"
-      ],
-      "addLabels": ["deploy:manually-deployed", "deploy:auto-merge"],
-      "prBodyNotes": "Auto-updating [home-assistant](https://github.com/home-assistant/core/releases) /cc @jnewland"
-    }
-  ]
-}
+      hostType: 'github',
+      matchHost: 'github.com',
+      username: process.env.RENOVATE_USERNAME,
+      token: process.env.RENOVATE_GITHUB_COM_TOKEN,
+    },
+  ],
+  postUpgradeTasks: {
+    commands: ['./script/sync-components'],
+    fileFilters: ['custom_components/**/*'],
+    executionMode: 'branch',
+  },
+  packageRules: [
+    {
+      matchPackageNames: ['homeassistant/home-assistant', 'homeassistant/core'],
+      addLabels: ['deploy:manually-deployed', 'deploy:auto-merge'],
+      prBodyNotes:
+        'Auto-updating [home-assistant](https://github.com/home-assistant/core/releases) /cc @jnewland',
+    },
+  ],
+};
