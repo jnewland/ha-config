b26912a92dbcb95ca138a140b62ba34dda87fe0d
Jesse Newland <jesse@jnewland.com>
2023-01-03 10:29:12 -0500

    unnecessary now

--- renovate.js
+++ renovate.js
@@ -1,5 +1,6 @@
 module.exports = {
   "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+  "username": "jnewland-renovate[bot]",
   "globalExtends": ["github>jnewland/.github"],
   "postUpgradeTasks": {
     "commands": ["./script/sync-components"],
@@ -15,9 +16,5 @@ module.exports = {
       "addLabels": ["deploy:manually-deployed", "deploy:auto-merge"],
       "prBodyNotes": "Auto-updating [home-assistant](https://github.com/home-assistant/core/releases) /cc @jnewland"
     }
-  ],
-  "hostRules": [{
-    "matchHost": "api.github.com",
-    "token": process.env.GITHUB_COM_TOKEN
-  }]
+  ]
 }
