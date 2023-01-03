7ab7aa565dd7eff1eca982648df5e3be160f9736
Jesse Newland <jesse@jnewland.com>
2023-01-03 09:30:27 -0500

    provide token to renovate

--- renovate.js
+++ renovate.js
@@ -1,5 +1,6 @@
-{
+module.exports = {
   "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+  "globalExtends": ["github>jnewland/.github"],
   "postUpgradeTasks": {
     "commands": ["./script/sync-components"],
     "fileFilters": ["custom_components/**/*"],
@@ -14,5 +15,9 @@
       "addLabels": ["deploy:manually-deployed", "deploy:auto-merge"],
       "prBodyNotes": "Auto-updating [home-assistant](https://github.com/home-assistant/core/releases) /cc @jnewland"
     }
-  ]
+  ],
+  "hostRules": [{
+    "matchHost": "api.github.com",
+    "token": process.env.GITHUB_COM_TOKEN
+  }]
 }
