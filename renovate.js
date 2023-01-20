d088e4313214ef1e048803891957a21b39a9e0d3
Jesse Newland <jesse@jnewland.com>
2023-01-19 20:16:33 -0600

    everything

--- renovate.js
+++ renovate.js
@@ -10,7 +10,7 @@ module.exports = {
   ],
   postUpgradeTasks: {
     commands: ['./script/sync-components'],
-    fileFilters: ['custom_components/**/*'],
+    fileFilters: ['**/**'],
     executionMode: 'branch',
   },
   packageRules: [
