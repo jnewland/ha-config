1b02119199d72349700f471fe1a6a8db2c55ec0c
jnewland-renovate <195354+jnewland-renovate[bot]@users.noreply.github.com>
2023-05-22 04:04:55 +0000

    chore(deps): update dependency sopelj/hass-ember-mug-component to v0.7.0

--- custom_components/ember_mug/diagnostics.py
+++ custom_components/ember_mug/diagnostics.py
@@ -1,13 +1,17 @@
 """Diagnostics support for Mug."""
 from __future__ import annotations
 
+import logging
 from typing import Any
 
+from bleak import BleakError
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from . import HassMugData
 from .const import DOMAIN
+
+logger = logging.getLogger(__name__)
 
 
 async def async_get_config_entry_diagnostics(
@@ -22,4 +26,20 @@ async def async_get_config_entry_diagnostics(
         "state": coordinator.data.liquid_state_display,
         "address": coordinator.mug.device.address,
     }
+    if coordinator.mug.debug is True:
+        services: dict[str, Any] | None = None
+        try:
+            services = await coordinator.mug.discover_services()
+        except BleakError as e:
+            logger.error("Failed to log services, %s", e)
+        if services is not None:
+            # Ensure bytes are converted into strings for serialization
+            for service in services.values():
+                for char in service["characteristics"].values():
+                    if (value := char["value"]) is not None:
+                        char["value"] = str(value)
+                    for desc in char["descriptors"]:
+                        if (value := desc["value"]) is not None:
+                            desc["value"] = str(value)
+            data["services"] = services
     return data
