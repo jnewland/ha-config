b80d18f28a276c1b3c6795f0fdc8034e4e9a439d
Jesse Newland <jnewland@urcomputeringpal.com>
2026-04-13 17:01:56 -0500

    defaults

--- renovate.js
+++ renovate.js
@@ -1,17 +1,16 @@
 module.exports = {
-  extends: ['github>urcomputeringpal/.github'],
-  allowedPostUpgradeCommands: ['./script/sync-components'],
+  extends: ["github>urcomputeringpal/.github"],
+  allowedPostUpgradeCommands: ["./script/sync-components"],
   postUpgradeTasks: {
-    commands: ['./script/sync-components'],
-    fileFilters: ['**/**'],
-    executionMode: 'branch',
+    commands: ["./script/sync-components"],
+    executionMode: "branch",
   },
   packageRules: [
     {
-      matchPackageNames: ['homeassistant/home-assistant', 'homeassistant/core'],
-      addLabels: ['deploy', 'deploy:auto-merge'],
+      matchPackageNames: ["homeassistant/home-assistant", "homeassistant/core"],
+      addLabels: ["deploy", "deploy:auto-merge"],
       prBodyNotes:
-        'Auto-updating [home-assistant](https://github.com/home-assistant/core/releases) /cc @jnewland',
-    }
+        "Auto-updating [home-assistant](https://github.com/home-assistant/core/releases) /cc @jnewland",
+    },
   ],
 };
